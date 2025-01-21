3e1bf3160baa2d2173892efd69c470fa8521c1dc
javiencm <javienchan17@gmail.com>
2025-01-20 20:42:54 -0800

    feat(week1): solve p3 & p4

--- week1/FindMiddle.java
+++ week1/FindMiddle.java
@@ -1,6 +1,8 @@
+package week1;
+
 /**
  *
- * @author ...
+ * @author JC
  */
 class Node {
     int data;
@@ -13,15 +15,26 @@ class Node {
 }
 
 public class FindMiddle {
-    
-    public static Node findMiddle(Node head) {
-        // WRITE YOUR CODE HERE!!!
 
-        return new Node(1); /// UPDATE THE RETURN STATEMENT ALSO!!!!
+    public static Node findMiddle(Node head) {
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return head;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
     }
 
     public static void main(String[] args) {
-        
+
         // SAMPLE CASE 1 || 1->2->3->4->5
         Node head = new Node(1);
         head.next = new Node(2);
@@ -30,8 +43,20 @@ public class FindMiddle {
         head.next.next.next.next = new Node(5);
         Node middle = findMiddle(head);
         System.out.println("Middle Node: " + (middle != null ? middle.data : "Empty List")); // EXPECTED RESULT: 3
-        
+
         // ADD YOUR TEST CASES HERE!!!
+        // Test Case 2: Even length list
+        Node head2 = new Node(1);
+        head2.next = new Node(2);
+        head2.next.next = new Node(3);
+        head2.next.next.next = new Node(4);
+        System.out.println("Middle (Even): " + findMiddle(head2).data); // Expected: 3
+
+        // Test Case 3: Single node
+        Node head3 = new Node(1);
+        System.out.println("Middle (Single): " + findMiddle(head3).data); // Expected: 1
+
+        // Test Case 4: Empty list
+        System.out.println("Middle (Empty): " + (findMiddle(null) == null ? "null" : "not null")); // Expected: null
     }
 }
-
--- week1/ReversePolishNotation.java
+++ week1/ReversePolishNotation.java
@@ -1,27 +1,50 @@
+package week1;
+
 /**
  *
- * @author ...
+ * @author JC
  */
 
 import java.util.Stack;
 
 public class ReversePolishNotation {
-    
+
     public static int evalRPN(String[] tokens) {
-        // WRITE YOUR CODE HERE!!!
-        return -1; // UPDATE THIS LINE OF CODE ALSO !!!!
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            switch (token) {
+                case "+":
+                    stack.push(stack.pop() + stack.pop());
+                    break;
+                case "-":
+                    int b = stack.pop();
+                    int a = stack.pop();
+                    stack.push(a - b);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * stack.pop());
+                    break;
+                case "/":
+                    int divisor = stack.pop();
+                    int dividend = stack.pop();
+                    stack.push(dividend / divisor);
+                    break;
+                default:
+                    stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
     }
 
     public static void main(String[] args) {
-        
+
         // SAMPLE CASE 1
-        String[] tokens1 = {"2", "1", "+", "3", "*"};
+        String[] tokens1 = { "2", "1", "+", "3", "*" };
         System.out.println("Result: " + evalRPN(tokens1)); // Output: 9
 
         // SAMPLE CASE 2
-        String[] tokens2 = {"4", "13", "5", "/", "+"};
+        String[] tokens2 = { "4", "13", "5", "/", "+" };
         System.out.println("Result: " + evalRPN(tokens2)); // Output: 6
 
     }
 }
-
